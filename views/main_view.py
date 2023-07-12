46d8cca196efdd50dd6cbbc1938b0df113708e82
jinseo <ghkdwlstj99@gmail.com>
2023-07-13 00:36:06 +0900

    feat: 질문 목록 템플릿 구현

--- views/main_view.py
+++ views/main_view.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from main.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
-def hello_world():
-    return "Hello, World"
+def index():
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('question/question_list.html', question_list=question_list)
