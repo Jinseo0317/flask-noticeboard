5d98816d82e3769d78f579d52d81c777bfc92b7f
jinseo <ghkdwlstj99@gmail.com>
2023-07-13 12:21:35 +0900

    feat: 질문 목록에 대한 템플릿 구현

--- views/main_view.py
+++ views/main_view.py
@@ -7,3 +7,8 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def index():
     question_list = Question.query.order_by(Question.create_date.desc())
     return render_template('question/question_list.html', question_list=question_list)
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
